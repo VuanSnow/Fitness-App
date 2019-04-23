fa53a7480b9b45f7021311a464615a3ebd1c79b6
Vuan <ldkradol@gmail.com>
2019-04-23 19:05:43 +0300

    Removed store subscription

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,11 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
-
+/*
 store.subscribe(() => {
   console.log("Store updated!");
   console.table(store.getState().customers);
 });
+*/
 
 export default store;
